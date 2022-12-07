08c7cc1ed87fac91599db0253442212cc31d6d2d
Luiz Kornrumpf <luiz.kornrumpf@gmail.com>
2022-12-07 15:58:07 +0100

    move App.test.js to __test__ folder

--- src/__test__/App.test.js
+++ src/__test__/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen, fireEvent } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import App from "./App";
+import App from "../App";
 
 test("Test if clear button has been render", () => {
   render(<App />);
