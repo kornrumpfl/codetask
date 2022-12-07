1779c6741743214f9d89162417280c8931025870
Luiz Kornrumpf <luiz.kornrumpf@gmail.com>
2022-12-07 14:39:17 +0100

    Add unit tests: - Test if clear button has been render - Test for first single uppercase character - Test for last single uppercase character - Test for mixed uppercase and lowercase characters - Test for full word - Test empty string - Test for numbers mixed with characters

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,56 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Test if clear button has been render", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByRole("button");
   expect(linkElement).toBeInTheDocument();
+});
+
+test("Test for first single uppercase character", () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText("type-something");
+  fireEvent.change(input, { target: { value: "A" } });
+  const sum = screen.getByTestId("sum");
+  expect(sum.textContent).toBe("1");
+});
+
+test("Test for last single uppercase character", () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText("type-something");
+  fireEvent.change(input, { target: { value: "Z" } });
+  const sum = screen.getByTestId("sum");
+  expect(sum.textContent).toBe("26");
+});
+
+test("Test for mixed uppercase and lowercase characters", () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText("type-something");
+  fireEvent.change(input, { target: { value: "Az" } });
+  const sum = screen.getByTestId("sum");
+  expect(sum.textContent).toBe("27");
+});
+
+test("Test for full word", () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText("type-something");
+  fireEvent.change(input, { target: { value: "Test" } });
+  const sum = screen.getByTestId("sum");
+  expect(sum.textContent).toBe("64");
+});
+
+test("Test empty string", () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText("type-something");
+  fireEvent.change(input, { target: { value: "Te st" } });
+  const sum = screen.getByTestId("sum");
+  expect(sum.textContent).toBe("64");
+});
+
+test("Test for numbers mixed with characters", () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText("type-something");
+  fireEvent.change(input, { target: { value: "I8v9a10n" } });
+  const sum = screen.getByTestId("sum");
+  expect(sum.textContent).toBe("46");
 });
